cf653d3643512425cee97fc45fdca6d78a8300db
Andry Saprigin <coffeei.2002@gmail.com>
2023-11-20 07:29:24 +0200

    NewIBMJava

---
+++ src/main/java/com/example/CurrencyModel.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import lombok.Data;
+
+@Data
+public class CurrencyModel {
+    public String CCY;
+    public String base_CCY;
+    public String buy;
+    public String sale;
+}
---
+++ src/main/java/com/example/config/BotConfig.java
@@ -0,0 +1,17 @@
+package com.example.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import lombok.Data;
+
+@Configuration
+@Data
+@PropertySource("application.properties")
+public class BotConfig {
+    @Value("${bot.name}")
+    private String botName;
+    @Value("@{bot.token}")
+    private String token;
+}
