62d8f5eb136be6f94189ab9629c6026e829f03cd
Andry Saprigin <coffeei.2002@gmail.com>
2023-11-27 21:12:30 +0200

    springframework not see a file

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,21 +7,18 @@ import com.example.config.BotInitializer;
 
 public class App {
     public static void main(String[] args) {
-        // try(AnnotationConfigApplicationContext appContext = new
-        // AnnotationConfigApplicationContext(BotConfig.class)) {
+        try (AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(BotConfig.class)) {
+            BotConfig botConfig = appContext.getBean("botConfigBean", BotConfig.class);
 
-        // } catch (Exception e) {
-        // System.out.println(e.getMessage());
-        // }
-
-        AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(BotConfig.class);
-        BotConfig botConfig = appContext.getBean("botConfigBean", BotConfig.class);
-
-        try {
-            BotInitializer initializer = new BotInitializer(new TelegramBot(botConfig));
-            initializer.init();
+            try {
+                BotInitializer initializer = new BotInitializer(new TelegramBot(botConfig));
+                initializer.init();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
     }
 }
--- src/main/java/com/example/config/BotConfig.java
+++ src/main/java/com/example/config/BotConfig.java
@@ -1,16 +1,14 @@
 package com.example.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import lombok.Data;
 
 @Configuration
 @Data
-@PropertySource("../../../../../../application.properties")
+@PropertySource("./application.properties")
 public class BotConfig {
     @Value("${bot.name}")
     public String botName;
